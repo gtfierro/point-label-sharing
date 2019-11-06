0b4b235067a77da65f57e71d285a3ec1bcc8f54f
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-11-06 12:47:57 -0800

    fix docker push command

--- container/build.sh
+++ container/build.sh
@@ -6,6 +6,6 @@ git checkout add-scanner
 cd frontend && npm install && npm run build
 popd
 docker build -t mortar/pointscan:latest .
-docker push mortar/pointscan:latest .
+docker push mortar/pointscan:latest
 echo -e "Scan with\ndocker run --name pointscan --rm mortar/pointscan:latest scan"
 echo -e "Run with\ndocker run -p 8080:8000 --name pointscan-web mortar/pointscan:latest"
