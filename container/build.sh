53b9efc9f42f8e71079839f28df895c862e1b298
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-11-07 11:10:36 -0800

    fix port

--- container/build.sh
+++ container/build.sh
@@ -8,4 +8,4 @@ popd
 docker build -t mortar/pointscan:latest .
 docker push mortar/pointscan:latest
 echo -e "Scan with\ndocker run --name pointscan --rm mortar/pointscan:latest scan"
-echo -e "Run with\ndocker run -p 8080:8000 --name pointscan-web mortar/pointscan:latest"
+echo -e "Run with\ndocker run -p 5000:5000 --name pointscan-web mortar/pointscan:latest"
