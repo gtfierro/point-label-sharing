aa90c3f2687f1585883ce6fa19c1252875e06b92
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-11-07 10:42:40 -0800

    fix import

--- pointscan/scan.py
+++ pointscan/scan.py
@@ -43,7 +43,7 @@ def scan(ip, dest):
 @main.command(help="Run webserver to clean/publish datasets")
 @click.option("--port", default=5000, help="webserver port")
 def web(port):
-    from pointscan.app import app
+    from app import app
     app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == '__main__':
